573f00cc0207c828f0ebd2004ea6a23dbeb223aa
Miha Šušteršič <shooshte@Mihas-MBP.si.sportradar.ag>
2016-12-28 14:37:52 +0100

    added Timer tests

--- app/tests/components/Timer.test.jsx
+++ app/tests/components/Timer.test.jsx
@@ -22,21 +22,23 @@ describe('Timer', () => {
     });
     it('should pause timer on paused status', (done) => {
       let timer = TestUtils.renderIntoDocument(<Timer/>);
-      timer.handleStatusChange('paused');
+      timer.handleStatusChange('started');
       setTimeout(() => {
+        timer.handleStatusChange('paused');
         expect(timer.state.count).toBe(1);
         expect(timer.state.timerStatus).toBe('paused');
         done();
-      }, 3001);
+      }, 1001);
     });
     it('should clear timer on stopped status', (done) => {
       let timer = TestUtils.renderIntoDocument(<Timer/>);
-      timer.handleStatusChange('stopped');
+      timer.handleStatusChange('started');
       setTimeout(() => {
+        timer.handleStatusChange('stopped');
         expect(timer.state.count).toBe(0);
         expect(timer.state.timerStatus).toBe('stopped');
         done();
-      }, 1001);
+      }, 3001);
     });
   });
 });
