c4f041e3927f73e898309be27dbab0721701833a
Miha Šušteršič <shooshte@Mihas-MBP.si.sportradar.ag>
2016-12-28 14:49:31 +0100

    updated Timer tests

--- app/tests/components/Timer.test.jsx
+++ app/tests/components/Timer.test.jsx
@@ -2,7 +2,6 @@ let expect = require('expect');
 let React = require('react');
 let ReactDOM = require('react-dom');
 let TestUtils = require('react-addons-test-utils');
-let $ = require('jQuery');
 
 let Timer = require('Timer');
 
@@ -14,6 +13,7 @@ describe('Timer', () => {
     it('should start timer on started status', (done) => {
       let timer = TestUtils.renderIntoDocument(<Timer/>);
       timer.handleStatusChange('started');
+      expect(timer.state.count).toBe(0);
       setTimeout(() => {
         expect(timer.state.count).toBe(1);
         expect(timer.state.timerStatus).toBe('started');
@@ -22,23 +22,25 @@ describe('Timer', () => {
     });
     it('should pause timer on paused status', (done) => {
       let timer = TestUtils.renderIntoDocument(<Timer/>);
+      timer.setState({count:10});
       timer.handleStatusChange('started');
+      timer.handleStatusChange('paused');
       setTimeout(() => {
-        timer.handleStatusChange('paused');
-        expect(timer.state.count).toBe(1);
+        expect(timer.state.count).toBe(10);
         expect(timer.state.timerStatus).toBe('paused');
         done();
       }, 1001);
     });
     it('should clear timer on stopped status', (done) => {
       let timer = TestUtils.renderIntoDocument(<Timer/>);
+      timer.setState({count: 10});
       timer.handleStatusChange('started');
+      timer.handleStatusChange('stopped');
       setTimeout(() => {
-        timer.handleStatusChange('stopped');
         expect(timer.state.count).toBe(0);
         expect(timer.state.timerStatus).toBe('stopped');
         done();
-      }, 3001);
+      }, 1001);
     });
   });
 });
