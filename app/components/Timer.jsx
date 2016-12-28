c202147559fdc6597773010d9239ac06482ec108
Miha Šušteršič <shooshte@Mihas-MBP.si.sportradar.ag>
2016-12-28 14:30:25 +0100

    added Timer component

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,11 +1,54 @@
 let React = require('react');
+let Clock = require('Clock');
+let Controls = require('Controls');
 
-let Timer = () => {
-  return (
-      <div>
-        <p>Timer.jsx</p>
-      </div>
-  )
-}
+let Timer = React.createClass ({
+  getInitialState: function() {
+    return {
+      count: 0,
+      timerStatus: 'stopped'
+    }
+  },
+  componentDidUpdate: function(prevProps, prevState) {
+    if(this.state.timerStatus !== prevState.timerStatus) {
+      switch (this.state.timerStatus) {
+        case 'started' :
+          this.startTimer();
+          break;
+        case 'stopped' :
+          this.setState({count: 0});
+        case 'paused':
+          clearInterval(this.timer);
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
+  startTimer: function() {
+    this.timer = setInterval(() => {
+      let newCount = this.state.count +1;
+      this.setState({
+        count: newCount
+      });
+    }, 1000);
+  },
+  handleStatusChange: function(newTimerStatus) {
+    this.setState({timerStatus: newTimerStatus});
+  },
+  render: function() {
+    let {count, timerStatus} = this.state;
+    return (
+        <div>
+          <h1 className="page-title">Timer App</h1>
+          <Clock totalSeconds={count}/>
+          <Controls countdownStatus={timerStatus} onStatusChange={this.handleStatusChange}/>
+        </div>
+    )
+  }
+});
 
 module.exports = Timer;
---
+++ app/tests/components/Timer.test.jsx
@@ -0,0 +1,42 @@
+let expect = require('expect');
+let React = require('react');
+let ReactDOM = require('react-dom');
+let TestUtils = require('react-addons-test-utils');
+let $ = require('jQuery');
+
+let Timer = require('Timer');
+
+describe('Timer', () => {
+  it('should exist', () => {
+    expect(Timer).toExist();
+  });
+  describe('handleStatusChange', () => {
+    it('should start timer on started status', (done) => {
+      let timer = TestUtils.renderIntoDocument(<Timer/>);
+      timer.handleStatusChange('started');
+      setTimeout(() => {
+        expect(timer.state.count).toBe(1);
+        expect(timer.state.timerStatus).toBe('started');
+        done();
+      }, 1001);
+    });
+    it('should pause timer on paused status', (done) => {
+      let timer = TestUtils.renderIntoDocument(<Timer/>);
+      timer.handleStatusChange('paused');
+      setTimeout(() => {
+        expect(timer.state.count).toBe(1);
+        expect(timer.state.timerStatus).toBe('paused');
+        done();
+      }, 3001);
+    });
+    it('should clear timer on stopped status', (done) => {
+      let timer = TestUtils.renderIntoDocument(<Timer/>);
+      timer.handleStatusChange('stopped');
+      setTimeout(() => {
+        expect(timer.state.count).toBe(0);
+        expect(timer.state.timerStatus).toBe('stopped');
+        done();
+      }, 1001);
+    });
+  });
+});
